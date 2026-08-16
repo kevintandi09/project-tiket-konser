d02d7ce3d319711c45153e69816bff1cb609a02c
martontan <kevin.tandi110@gmail.com>
2026-08-16 14:41:21 +0700

    membuat data konser sementara

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,3 +1,10 @@
 export function initHero(element) {
-  console.log("Hero initialized");
+  const eventCategory = document.getElementById("event-category");
+  const eventTitle = document.getElementById("event-title");
+  const eventDate = document.getElementById("date");
+  const eventLocation = document.getElementById("location");
+  const buyTicketsButton = document.getElementById("buy-tickets");
+  const learnMoreButton = document.getElementById("learn-more");
+
+  
 };
---
+++ data/datas.js
@@ -0,0 +1,23 @@
+const konserDatas =[
+  {
+    Category: "Jakarta Fair Kemayoran 2026",
+    Title: "Jakaerta fair music concert",
+    Date: "11 Juni - 12 Juli 2026",
+    Location: "Jiexpo kemayoran, Jakarta",  
+    image: "https://asset.kompas.com/crops/l5qpurcUVDq0WAsFLRJnONg9D4g=/0x0:0x0/1200x1200/data/photo/2026/06/11/6a2aca543f30c.jpeg"
+  },
+  {
+    Category: "Svara Festival ",
+    Title: "Bazaar Music Festival",
+    Date: "19 Juli - 20 Juli 2026",
+    Location: "Bandung, Indonesia",
+    image: "https://cdns.klimg.com/resized/1200x600/p/headline/svara-fest-2025-siap-digelar-ada-maliq--a54883.jpg"
+  }, 
+  {
+    Category: "Melophile Festival",
+    Title: "Melophile Music Festival : Dewa 19",
+    Date: "11 April 2026",
+    Location: "Lapangan Benteng, Medan",
+    image: "https://cdn.rakyatnews.net/wp-content/uploads/2025/11/intip-kemeriahan-konser-dewa-19-di-bandung-pLWIV2eP2S-1.jpeg"
+  }
+]
