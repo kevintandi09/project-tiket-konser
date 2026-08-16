7a31aa4b90185d9a0d63e73f9f0d1b9a1350e1d1
martontan <kevin.tandi110@gmail.com>
2026-08-16 15:56:17 +0700

    membuat code menggunakan js agar text berubah berdasarkan data

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,10 +1,20 @@
+import { konserDatas } from "/data/datas.js"
+
 export function initHero(element) {
+  const hero = document.getElementById("hero");
   const eventCategory = document.getElementById("event-category");
   const eventTitle = document.getElementById("event-title");
   const eventDate = document.getElementById("date");
   const eventLocation = document.getElementById("location");
-  const buyTicketsButton = document.getElementById("buy-tickets");
-  const learnMoreButton = document.getElementById("learn-more");
+  const konserData = konserDatas[0]; 
 
-  
+  hero.style.backgroundImage = `url('${konserData.image}')`;
+  hero.style.backgroundRepeat = "no-repeat";
+  hero.style.backgroundSize = "cover";
+  hero.style.backgroundPosition = "center";
+  eventCategory.textContent = konserData.category;
+  eventTitle.textContent = konserData.title;
+  eventDate.lastChild.textContent = `${konserData.date}`;
+  eventLocation.lastChild.textContent = `${konserData.location}`;
+  hero.style.color = konserData.textColor;
 };
--- data/datas.js
+++ data/datas.js
@@ -1,23 +1,26 @@
 const konserDatas =[
   {
-    Category: "Jakarta Fair Kemayoran 2026",
-    Title: "Jakaerta fair music concert",
-    Date: "11 Juni - 12 Juli 2026",
-    Location: "Jiexpo kemayoran, Jakarta",  
-    image: "https://asset.kompas.com/crops/l5qpurcUVDq0WAsFLRJnONg9D4g=/0x0:0x0/1200x1200/data/photo/2026/06/11/6a2aca543f30c.jpeg"
+    category: "Jakarta Fair Kemayoran 2026",
+    title: "Jakarta fair music concert",
+    date: "11 Juni - 12 Juli 2026",
+    location: "Jiexpo kemayoran, Jakarta",  
+    image: "https://asset.kompas.com/crops/l5qpurcUVDq0WAsFLRJnONg9D4g=/0x0:0x0/1200x1200/data/photo/2026/06/11/6a2aca543f30c.jpeg",
+    textColor: '#f584ff'
   },
   {
-    Category: "Svara Festival ",
-    Title: "Bazaar Music Festival",
-    Date: "19 Juli - 20 Juli 2026",
-    Location: "Stadiun basket ball GBK, Jakarta",
-    image: "https://cdns.klimg.com/resized/1200x600/p/headline/svara-fest-2025-siap-digelar-ada-maliq--a54883.jpg"
+    category: "Svara Festival ",
+    title: "Bazaar Music Festival",
+    date: "19 Juli - 20 Juli 2026",
+    location: "Stadiun basketball GBK, Jakarta",
+    image: "https://image.idntimes.com/post/20250722/upload_f3ac02f4e72e5a0838aea48668695eaa_e1e3a920-2cad-4c4d-b617-8386702c9999.jpeg",
   }, 
   {
-    Category: "Melophile Festival",
-    Title: "Melophile Music Festival : Dewa 19",
-    Date: "11 April 2026",
-    Location: "Lapangan Benteng, Medan",
-    image: "https://cdn.rakyatnews.net/wp-content/uploads/2025/11/intip-kemeriahan-konser-dewa-19-di-bandung-pLWIV2eP2S-1.jpeg"
+    category: "Melophile Festival",
+    title: "Melophile Music Festival : Dewa 19",
+    date: "11 April 2026",
+    location: "Lapangan Benteng, Medan",
+    image: "https://cdn.rakyatnews.net/wp-content/uploads/2025/11/intip-kemeriahan-konser-dewa-19-di-bandung-pLWIV2eP2S-1.jpeg",
   }
-]
+];
+
+export {konserDatas};
